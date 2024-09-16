1e231d2f155027f3fee6a334bc226b062c8f9ae5
IAN <julian63k@gmail.com>
2024-09-16 15:24:05 -0500

    home finish?

--- BulletInBoard/src/components/Home.tsx
+++ BulletInBoard/src/components/Home.tsx
@@ -1,7 +1,7 @@
-// import { useEffect, useState } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// import { useState } from 'react';
 import noti from '../assets/img/Notificationicon.png';
-import { ContainerHeader, Divider, HeaderHome, MainHome, WrapperElements, ContentCard } from '../styledComponents/containers';
+import { ContainerHeader, HeaderHome, MainHome, WrapperElements } from '../styledComponents/containers';
 import MenuUl from './containers/MenuUl';
 import SliderHome from './slider/SliderHome';
 import Card from '@mui/material/Card';
@@ -9,10 +9,10 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CardActionArea from '@mui/material/CardActionArea';
 import '../styles/home.css'
-import { Typography } from '@mui/material';
-// import { onAuthStateChanged } from 'firebase/auth';
-// import { auth } from '../firebase/firebaseConfig';
-// import { useDispatch } from 'react-redux';
+import location from '../assets/img/location.svg';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase/FirebaseConfig';
+import { useDispatch } from 'react-redux';
 
 const Home = () => {
 
@@ -21,29 +21,25 @@ const Home = () => {
         photoURL: string | null | undefined;
     }
 
-    const [activeButton, setActiveButton] = useState(0);
     const [isMenuVisible, setIsMenuVisible] = useState(false);
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const [user, setUser] = useState({} as User);
 
-    const handleClick = (index: number) => {
-        setActiveButton(index);
-    };
 
     const toggleMenu = () => {
         setIsMenuVisible(!isMenuVisible);
     };
 
-    // useEffect(() => {
-    //     onAuthStateChanged(auth, (user) => {
-    //         if (user) {
-    //             console.log('usuario loggeado en', user);
-    //             setUser(user);
-    //         } else {
-    //             console.log('usuario no loggeado');
-    //         }
-    //     });
-    // }, [dispatch]);
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log('usuario loggeado en', user);
+                setUser(user);
+            } else {
+                console.log('usuario no loggeado');
+            }
+        });
+    }, [dispatch]);
 
 
     return (
@@ -70,29 +66,154 @@ const Home = () => {
                 <SliderHome />
             </HeaderHome>
             <MainHome>
-                <div className='vie'>
-                    <p>Recently viewed</p>
-                    <p> ( ) </p>
+                <div className='d-flex-column'>
+                    <div className='vie'>
+                        <p>Recently viewed</p>
+                        <p> ( ) </p>
+                    </div>
+                    <div className='d-flex-row gap-1em'>
+                        <Card sx={{ maxWidth: 345, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
+                            <CardActionArea >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <div>
+                                        <h3 style={{ color: '#474747' }}>820$</h3>
+                                        <div className='d-flex-row j-content-space-between card_content'>
+                                            <p>Hp notebook 2014</p>
+                                            <p>
+                                                <img src={location} alt="" />
+                                                12 km
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                        <Card sx={{ maxWidth: 345, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
+                            <CardActionArea >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <div>
+                                        <h3 style={{ color: '#474747' }}>820$</h3>
+                                        <div className='d-flex-row j-content-space-between card_content'>
+                                            <p>Hp notebook 2014</p>
+                                            <p>
+                                                <img src={location} alt="" />
+                                                12 km
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </div>
                 </div>
-                <Card sx={{ maxWidth: 345, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
-                    <CardActionArea >
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
-                            alt="green iguana"
-                        />
-                        <CardContent>
-                            <div>
-                                <p>820$</p>
-                                <div className='d-flex-row'>
-                                    <p>name</p>
-                                    <p>12 km</p>
-                                </div>
-                            </div>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
+                <div className='d-flex-column'>
+                    <div className='vie'>
+                        <p>Recommended to you</p>
+                    </div>
+                    <div className='d-flex flex-warp'>
+                        <Card sx={{ maxWidth: 170, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
+                            <CardActionArea >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <div>
+                                        <h3 style={{ color: '#474747' }}>820$</h3>
+                                        <div className='d-flex-row j-content-space-between card_content'>
+                                            <p>Hp notebook 2014</p>
+                                            <p>
+                                                <img src={location} alt="" />
+                                                12 km
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                        <Card sx={{ maxWidth: 170, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
+                            <CardActionArea >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <div>
+                                        <h3 style={{ color: '#474747' }}>820$</h3>
+                                        <div className='d-flex-row j-content-space-between card_content'>
+                                            <p>Hp notebook 2014</p>
+                                            <p>
+                                                <img src={location} alt="" />
+                                                12 km
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                        <Card sx={{ maxWidth: 170, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
+                            <CardActionArea >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <div>
+                                        <h3 style={{ color: '#474747' }}>820$</h3>
+                                        <div className='d-flex-row j-content-space-between card_content'>
+                                            <p>Hp notebook 2014</p>
+                                            <p>
+                                                <img src={location} alt="" />
+                                                12 km
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                        <Card sx={{ maxWidth: 170, background: 'none', border: 0, borderRadius: '.8em', boxShadow: 'none', color: '#000', backgroundColor: '#ebfff6' }}>
+                            <CardActionArea >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://d34vmoxq6ylzee.cloudfront.net/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/3/43K30LT-1_T1679066178.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <div>
+                                        <h3 style={{ color: '#474747' }}>820$</h3>
+                                        <div className='d-flex-row j-content-space-between card_content'>
+                                            <p>Hp notebook 2014</p>
+                                            <p>
+                                                <img src={location} alt="" />
+                                                12 km
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </div>
+                </div>
+
             </MainHome>
         </WrapperElements>
     )
